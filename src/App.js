515e4911701274c707f514f31d873e41433b55c5
Sharan-ram <sharanram1993@gmail.com>
2020-08-22 15:44:00 +0530

    Connect to exchange rates API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const CURRENCY_EXCHANGE_URL = "https://api.exchangeratesapi.io/latest";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [currencyOptions, setCurrencyOptions] = useState({});
+  const [sourceCurrency, setSourceCurrency] = useState("EUR");
+
+  console.log("currencyOptions", currencyOptions);
+  console.log("sourceCurrency", sourceCurrency);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        const url = `${CURRENCY_EXCHANGE_URL}?base=${sourceCurrency}`;
+        const {
+          data: { base, rates },
+        } = await axios.get(url);
+
+        setSourceCurrency(base);
+        setCurrencyOptions(rates);
+        setIsLoading(false);
+      } catch (e) {
+        console.error(e.message);
+        setIsLoading(false);
+      }
+    })();
+  }, [sourceCurrency]);
+
   return <div>Hello</div>;
 }
 
